091afb6db7693be01e75674fbe2da34c74a02280
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-01-17 10:43:54 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let m = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr <10) hr = '0' + hr;
+
+    if (m <10) m = '0' + m;
+
+    if (s < 10) s = '0'+ s;
+
+    horas.textContent = hr;
+    minutos.textContent = m;
+    segundos.textContent = s;
+})
+
